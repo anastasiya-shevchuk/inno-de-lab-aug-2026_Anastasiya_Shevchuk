a496907ea9f336716012f92265a0098fe73535e8
Анастасия Шевчук <shevchuk09061998@gmail.com>
2026-09-05 09:39:47 +0300

    homework 09: fixed

--- python/homework_9/homework_9_01.py
+++ python/homework_9/homework_9_01.py
@@ -2,35 +2,35 @@ class Trainee:
     def __init__(self, name: str, surname: str, score: int = 0, passing_grade: int = 10):
         self.name = name
         self.surname = surname
-        self.__score = score
+        self.score = score
         self.passing_grade = passing_grade
 
     @property
-    def score(self):
+    def score(self) -> int:
         return self.__score
 
     @score.setter
-    def score(self, new_score):
-        if type(new_score) != int:
-            raise TypeError (f"Expected value of type int, got {type(new_score)}")
+    def score(self, new_score:int) -> None:
+        if not isinstance(new_score, int):
+            raise TypeError (f"Expected value of type int, got {type(new_score).__name__}")
         if new_score < 0:
             raise ValueError ("The score shouldn't be less than 0!")
         self.__score = new_score
 
 
-    def do_homework(self):
+    def do_homework(self) -> None:
         """Increases score by 1"""
         self.score += 1
 
-    def miss_homework(self):
+    def miss_homework(self) -> None:
         """Decreases score by 1"""
         self.score -= 1
 
-    def visit_lecture(self):
+    def visit_lecture(self) -> None:
         """Increases score by 1"""
         self.score += 1
 
-    def miss_lecture(self):
+    def miss_lecture(self) -> None:
         """Decreases score by 1"""
         self.score -= 1
 
@@ -49,6 +49,13 @@ if __name__ == "__main__":
     print(f"Баллы: {trainee.score}, Прошел курс: {trainee.is_passing()}")
     # 4. Проверка валидации (попытка задать неверный тип или отрицательное значение)
     try:
+        trainee.score = "-5"
+    except TypeError as x:
+        print(f"Ошибка типа: {x}")
+
+    try:
         trainee.score = -5
     except ValueError as e:
         print(f"Ошибка: {e}")
+    except TypeError as x:
+        print(f"Ошибка типа: {x}")
